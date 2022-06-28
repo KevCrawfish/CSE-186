7ad87b37f01ee1f71b664f2e80eb423db6a422b6
KevCrawfish <kemcrawf@ucsc.edu>
2022-06-27 22:15:34 -0700

    head tags

--- Assignment3/Templater.js
+++ Assignment3/Templater.js
@@ -3,12 +3,30 @@
  */
 class Templater {
   /**
+   * Create a templater
+   * @param {string} template - A {{ }} tagged table header and
+   * data elements
+   */
+     constructor(template) {
+      if (template === undefined) {
+        return;
+      }
+      this.tag = template;
+    }
+
+  /**
    * Replace the contents of {{ }} tagged table header and data
    * elements in document with values found in the supplied JSON
    * @param {object} document
    * @param {string} json with propeties matching tags in document
    */
   byTag(document, json) {
+    for (let i = 0; i < this.tag.length; i++){
+      let str = this.tag[i].outerHTML.match(/(?<=\{\{)[\w]+(?=\}\})/);
+      let regex = new RegExp('(?<=' + str + '\\"\\:\\")[\\w\\s]+(?=\\"\\,)', 'g');
+      let find = json.match(regex);
+      this.tag[i].innerHTML = this.tag[i].innerHTML.replace(/\{\{[\w]+\}\}/, find);
+    }
   }
 
   /**
@@ -19,6 +37,11 @@ class Templater {
    */
   byId(document, json) {
   }
+}
+
+function funcByTag() {
+  let t = new Templater(document.getElementsByTagName('th'));
+  t.byTag(document, document.getElementById('json').value);
 }
 
 // To satisfy linter rules
