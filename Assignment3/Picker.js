bc0f00fca0f3485c1a0220b1f0ef3ec774261772
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-06-30 00:10:06 -0700

    safety push

--- Assignment3/Picker.js
+++ Assignment3/Picker.js
@@ -10,5 +10,61 @@ class Picker {
   }
 }
 
+/**
+* Replace contents of picker with current month
+* @param {object} document
+* @param {array} months array with list of months
+*/
+function currentMonth(document, months) {
+  const date = new Date();
+  const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0);
+  const lastMDay = new Date(date.getFullYear(), date.getMonth(), 0);
+  document.getElementById('month').textContent = months[date.getMonth()];
+  document.getElementById('year').textContent = date.getFullYear();
+  date.setDate(1);
+  let d = date.getDay();
+  let j = 0;
+  const boundDay = new Date(date.getFullYear(), date.getMonth(), 1-d);
+  for (i = boundDay.getDate(); i <= lastMDay.getDate(); i++) {
+    document.getElementById('d'+j++).textContent = i;
+  }
+
+  for (i = 1; i <= lastDay.getDate(); i++) {
+    document.getElementById('d'+d++).textContent = i;
+  }
+}
+
+/**
+* Replace contents of innerHTML with next month
+* @param {object} document
+* @param {array} months array with list of months
+*/
+function nextMonth(document, months) {
+  console.log('ok');
+}
+
+/**
+* Replace contents of innerHTML with next month
+* @param {object} document
+* @param {array} months array with list of months
+*/
+function prevMonth(document, months) {
+  console.log('yeah');
+}
+
+window.addEventListener('DOMContentLoaded', function(e) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'];
+  currentMonth(document, months);
+  document.getElementById('next').addEventListener('click', function(e) {
+    nextMonth(document, months);
+  });
+  document.getElementById('next').className = 'enabledButton';
+  document.getElementById('prev').addEventListener('click', function(e) {
+    prevMonth(document, months);
+  });
+  document.getElementById('prev').className = 'enabledButton';
+});
+
 // To satisfy linter rules
 new Picker();
--- Assignment3/Templater.js
+++ Assignment3/Templater.js
@@ -7,12 +7,12 @@ class Templater {
    * @param {string} template - A {{ }} tagged table header and
    * data elements
    */
-     constructor(template) {
-      if (template === undefined) {
-        return;
-      }
-      this.tag = template;
+  constructor(template) {
+    if (template === undefined) {
+      return;
     }
+    this.tag = template;
+  }
 
   /**
    * Replace the contents of {{ }} tagged table header and data
@@ -21,14 +21,17 @@ class Templater {
    * @param {string} json with propeties matching tags in document
    */
   byTag(document, json) {
-    for (let i = 0; i < this.tag.length; i++){
-      let str = this.tag[i].outerHTML.match(/(?<=\{\{)[\w]+(?=\}\})/);
-      let regex = new RegExp('(?<=' + str + '\\"\\:\\")[\\w\\s.]+(?=\\")', 'g');
-      let find = json.match(regex);
+    for (let i = 0; i < this.tag.length; i++) {
+      const str = this.tag[i].outerHTML.match(/(?<=\{\{)[\w]+(?=\}\})/);
+      const regex = new RegExp(
+          '(?<=' + str + '\\"\\:\\")[\\w\\s.]+(?=\\")', 'g');
+      const find = json.match(regex);
       if (find === null) {
-        this.tag[i].innerHTML = this.tag[i].innerHTML.replace(/\{\{[\w]+\}\}/, '');
+        this.tag[i].innerHTML = this.tag[i].innerHTML.replace(
+            /\{\{[\w]+\}\}/, '');
       } else {
-        this.tag[i].innerHTML = this.tag[i].innerHTML.replace(/\{\{[\w]+\}\}/, find);
+        this.tag[i].innerHTML = this.tag[i].innerHTML.replace(
+            /\{\{[\w]+\}\}/, find);
       }
     }
   }
@@ -40,27 +43,15 @@ class Templater {
    * @param {string} json with propeties matching element ids in document
    */
   byId(document, json) {
-    let str = this.tag.match(/(?<=\")[A-Z][0-9]+(?=\"\:)/g);
-    let content = this.tag.match(/(?<=\")[A-Z][0-9]+(?=\"\:)/g);
-    for (let i = 0; i < str.length; i++){
-      let id = document.getElementById(str[i]);
-      let regex = new RegExp('(?<=' + str[i] + '\\"\\:\\")[\\w\\s.]+(?=\\")', 'g');
-      let find = this.tag.match(regex);
+    const str = this.tag.match(/(?<=\")[A-Z][0-9]+(?=\"\:)/g);
+    for (let i = 0; i < str.length; i++) {
+      const id = document.getElementById(str[i]);
+      const regex = new RegExp(
+          '(?<=' + str[i] + '\\"\\:\\")[\\w\\s.]+(?=\\")', 'g');
+      const find = this.tag.match(regex);
       id.textContent = find;
     }
   }
-}
-
-function funcByTag() {
-  let t = new Templater(document.getElementsByTagName('th'));
-  let t2 = new Templater(document.getElementsByTagName('td'));
-  t.byTag(document, document.getElementById('json').value);
-  t2.byTag(document, document.getElementById('json').value);
-}
-
-function funcById() {
-  let t = new Templater(document.getElementById('json').value);
-  t.byId(document, document.getElementById('json').value);
 }
 
 // To satisfy linter rules
