3f77e640a1c93159c4fd934f4de92c18a24791b8
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-06-30 18:44:05 -0700

    stuff

--- Assignment3/Picker.js
+++ Assignment3/Picker.js
@@ -14,23 +14,30 @@ class Picker {
 * Replace contents of picker with current month
 * @param {object} document
 * @param {array} months array with list of months
+* @param {Date} date current date
 */
-function currentMonth(document, months) {
-  const date = new Date();
+function setDate(document, months, date) {
+  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
   const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0);
-  const lastMDay = new Date(date.getFullYear(), date.getMonth(), 0);
+  const prevMonth = new Date(date.getFullYear(), date.getMonth(), 0);
   document.getElementById('month').textContent = months[date.getMonth()];
   document.getElementById('year').textContent = date.getFullYear();
-  date.setDate(1);
-  let d = date.getDay();
+  let d = firstDay.getDay();
   let j = 0;
   const boundDay = new Date(date.getFullYear(), date.getMonth(), 1-d);
-  for (i = boundDay.getDate(); i <= lastMDay.getDate(); i++) {
+  for (let i = boundDay.getDate(); i <= prevMonth.getDate(); i++) {
+    if (boundDay.getDate() != 1) {
+      document.getElementById('d'+j).classList.add('grey');
+    }
     document.getElementById('d'+j++).textContent = i;
   }
-
-  for (i = 1; i <= lastDay.getDate(); i++) {
+  for (let i = 1; i <= lastDay.getDate(); i++) {
     document.getElementById('d'+d++).textContent = i;
+  }
+  j = 1;
+  for (d; d <=41; d++) {
+    document.getElementById('d'+d).classList.add('grey');
+    document.getElementById('d'+d).textContent = j++;
   }
 }
 
@@ -38,30 +45,52 @@ function currentMonth(document, months) {
 * Replace contents of innerHTML with next month
 * @param {object} document
 * @param {array} months array with list of months
+* @param {Date} date current date
 */
-function nextMonth(document, months) {
-  console.log('ok');
+function nextMonth(document, months, date) {
+  for (let d = 0; d <= 41; d++) {
+    document.getElementById('d'+d).removeAttribute('class');
+  }
+  const thisMonth = new Date(date.getFullYear(), date.getMonth()+1, 1);
+  date.setDate(thisMonth.getDate());
+  date.setMonth(thisMonth.getMonth());
+  if (date.getFullYear() !== thisMonth.getFullYear()) {
+    date.setFullYear(thisMonth.getFullYear());
+  }
+  setDate(document, months, date);
 }
 
 /**
 * Replace contents of innerHTML with next month
 * @param {object} document
 * @param {array} months array with list of months
+* @param {Date} date current date
 */
-function prevMonth(document, months) {
-  console.log('yeah');
+function prevMonth(document, months, date) {
+  for (let d = 0; d <= 41; d++) {
+    document.getElementById('d'+d).removeAttribute('class');
+  }
+  const thisMonth = new Date(date.getFullYear(), date.getMonth()-1, 1);
+  date.setDate(thisMonth.getDate());
+  date.setMonth(thisMonth.getMonth());
+  if (date.getFullYear() !== thisMonth.getFullYear()) {
+    date.setFullYear(thisMonth.getFullYear());
+  }
+  setDate(document, months, date);
 }
 
 window.addEventListener('DOMContentLoaded', function(e) {
   const months = ['January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'];
-  currentMonth(document, months);
+  const date = new Date();
+  document.getElementById('d'+(date.getDate()+2)).classList.add('pick');
+  setDate(document, months, date);
   document.getElementById('next').addEventListener('click', function(e) {
-    nextMonth(document, months);
+    nextMonth(document, months, date);
   });
   document.getElementById('next').className = 'enabledButton';
   document.getElementById('prev').addEventListener('click', function(e) {
-    prevMonth(document, months);
+    prevMonth(document, months, date);
   });
   document.getElementById('prev').className = 'enabledButton';
 });
--- Assignment3/picker.test.js
+++ Assignment3/picker.test.js
@@ -4,10 +4,21 @@ const puppeteer = require('puppeteer');
  * @param {number} relative months ahead or behind the current date
  * @return {date} first day of the relative month
  */
-function firstDay(relative) {
+function firstDayNext(relative) {
   const date = new Date();
   date.setDate(1);
   date.setMonth(date.getMonth()+relative);
+  return date.getDay();
+}
+
+/**
+ * @param {number} relative months ahead or behind the current date
+ * @return {date} first day of the relative month
+ */
+ function firstDayPrev(relative) {
+  const date = new Date();
+  date.setDate(1);
+  date.setMonth(date.getMonth()-relative);
   return date.getDay();
 }
 
@@ -36,7 +47,7 @@ test('Next Months', async () => {
   for (let i = 0; i < relative; i++) {
     await page.click('#next');
   }
-  const elem = await page.$('#d'+firstDay(relative));
+  const elem = await page.$('#d'+firstDayNext(relative));
   const cont = await (await elem.getProperty('textContent')).jsonValue();
   expect(cont).toBe('1');
 });
@@ -46,3 +57,18 @@ test('Next Months', async () => {
 // Add your tests for the Advanced requirement below here
 //
 // //////////////////////////////////////////////////////////
+
+// Clicks the next button a random number of times then checks the first
+// day of the displayed month is correct.  For example, July 1, 2021 falls
+// on a Thursday, the 5h day of the week when weeks start on Sundays.
+test('Previous Months', async () => {
+  const page = await browser.newPage();
+  await page.goto(`file://${__dirname}/picker.html`);
+  const relative = Math.max(1, Math.floor(Math.random()*28));
+  for (let i = 0; i < relative; i++) {
+    await page.click('#prev');
+  }
+  const elem = await page.$('#d'+firstDayPrev(relative));
+  const cont = await (await elem.getProperty('textContent')).jsonValue();
+  expect(cont).toBe('1');
+});
