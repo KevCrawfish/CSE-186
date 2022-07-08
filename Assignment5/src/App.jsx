b2bd41660229b27e447c97b0319e080efcb674aa
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-07-07 17:12:20 -0700

    push

--- Assignment5/src/App.jsx
+++ Assignment5/src/App.jsx
@@ -25,6 +25,7 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+ * Lecture RwdReactHooks example code
  */
 
 import React from 'react';
