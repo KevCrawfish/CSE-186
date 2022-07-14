5eef2ac3cf7815197631390d14a21bd1a714f9b2
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-07-13 18:24:20 -0700

    get all need to remove content

--- Assignment7/src/mail.js
+++ Assignment7/src/mail.js
@@ -1,7 +1,36 @@
 
 const db = require('../test/db');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
+selectMails = async (box) => {
+  let select = 'SELECT * FROM mail';
+  if (box) {
+    select += ` WHERE mailbox ~* $1`
+  }
+  const query = {
+    text: select,
+    values: box ? [ `${box}` ] : [ ]
+  };
+  const { rows } = await pool.query(query);
+  const mails = [];
+  for (const row of rows) {
+    mails.push({name: row.mailbox, mail: row.mail});
+  }
+  return mails;
+}
+
 exports.getAll = async (req, res) => {
+  const mail = await selectMails(req.query.mailbox);
+  res.status(200).json(mail);
 };
 
 exports.getById = async (req, res) => {
