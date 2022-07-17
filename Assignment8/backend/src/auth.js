864bc66dc9b7456ad741cc913b3844175d032174
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-07-16 20:39:02 -0700

    init push: git status

---
+++ Assignment8/backend/src/auth.js
@@ -0,0 +1,22 @@
+/*
+#######################################################################
+#
+# Copyright (C) 2020-2022 David C. Harrison. All right reserved.
+#
+# You may not use, distribute, publish, or modify this code without 
+# the express written permission of the copyright holder.
+#
+#######################################################################
+*/
+
+const jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
+
+exports.login = async (req, res) => {
+
+};
+
+exports.check = (req, res, next) => {
+
+};
+
