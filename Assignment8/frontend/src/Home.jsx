27e604f75b17da5da916ba2d816966c18b1fb1a5
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-07-18 15:59:52 -0700

    added mui stuff

--- Assignment8/frontend/src/Home.jsx
+++ Assignment8/frontend/src/Home.jsx
@@ -1,4 +1,39 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+/**
+ * app bar from mui
+ * @return {appbar}
+ */
+function ButtonAppBar() {
+  return (
+    <Box sx={{flexGrow: 1}}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{mr: 2}}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+            CSE 186
+          </Typography>
+          <Button color="inherit">Log-Out</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
 
 const fetchMails = (setMails, setError, mailbox) => {
   const item = localStorage.getItem('user');
@@ -44,7 +79,7 @@ function Home() {
 
   return (
     <div>
-      <h2 id='welcome'>CSE186</h2>
+      <ButtonAppBar></ButtonAppBar>
       <p/>
       <table id='mails'>
         <tbody>
