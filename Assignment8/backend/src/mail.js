129636509b23d84422fd780d2a65033e6b5efdec
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-07-18 13:33:17 -0700

    src file

---
+++ Assignment8/backend/src/mail.js
@@ -0,0 +1,46 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const secrets = require('../data/secrets.json');
+const {Pool} = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
+selectMails = async (req) => {
+  let select = 'SELECT * FROM mail';
+  let value = [];
+  if (req.query.mailbox) {
+    select += ` WHERE id = (`;
+    select += ` SELECT id FROM mailbox`;
+    select += ` WHERE lower(boxname) = $1`;
+    select += ` AND owned_by = $2)`;
+    value = [`${req.query.mailbox}`, `${req.user.email}`];
+  }
+  const query = {
+    text: select,
+    values: value,
+  };
+  const {rows} = await pool.query(query);
+  const mails = [];
+  for (const row of rows) {
+    row.mail.id = row.uqid;
+    mails.push({mail: row.mail});
+  }
+
+  return mails;
+};
+
+exports.getAll = async (req, res) => {
+  const mail = await selectMails(req);
+  if (mail.length !== 0) {
+    res.status(200).json(mail);
+  } else {
+    res.status(404).send();
+  }
+};
+
