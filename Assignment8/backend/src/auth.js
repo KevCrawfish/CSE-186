b105dd5e919b53f9cecfe1060b8ab9266129779a
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-07-18 15:53:00 -0700

    stuff

--- Assignment8/backend/src/auth.js
+++ Assignment8/backend/src/auth.js
@@ -44,17 +44,13 @@ exports.login = async (req, res) => {
 
 exports.check = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, secrets.accessToken, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-      req.user = user;
-      next();
-    });
-  } else {
-    res.sendStatus(401);
-  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, secrets.accessToken, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
 };
 
