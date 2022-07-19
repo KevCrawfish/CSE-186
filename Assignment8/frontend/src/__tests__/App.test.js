acc9fcca3eb0ec4078f02c6a05dfa7c6ebccc423
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-07-18 17:26:05 -0700

    gave up on frontend tests

--- Assignment8/frontend/src/__tests__/App.test.js
+++ Assignment8/frontend/src/__tests__/App.test.js
@@ -2,9 +2,26 @@ import {render} from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import App from '../App';
+import Home from '../Home';
+
+beforeEach(() => {
+  Object.defineProperty(window, 'localStorage', {
+    value: {
+      getItem: jest.fn(() => null),
+      setItem: jest.fn(() => null)
+    },
+    writable: true
+  });
+});
 
 /**
  */
 test('App Renders', async () => {
   render(<App />);
+});
+
+/**
+ */
+test('Home Renders', async () => {
+  render(<Home />);
 });
