18ce04497a371ff11da034da8b7e5bf2ae3561b4
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-07-20 12:51:39 -0700

    e2e test

---
+++ Assignment8/e2e/tests/e2e.test.js
@@ -0,0 +1,69 @@
+const puppeteer = require('puppeteer');
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+require('dotenv').config();
+const app = require('../../backend/src/app');
+
+let backend;
+let frontend;
+let browser;
+let page;
+
+beforeAll(() => {
+  backend = http.createServer(app);
+  backend.listen(3011, () => {
+    console.log('Backend Running at http://localhost:3011');
+  });
+  frontend = http.createServer(
+    express()
+      .use('/v0', createProxyMiddleware({ 
+        target: 'http://localhost:3011/',
+        changeOrigin: true}))
+      .use('/static', express.static(
+        path.join(__dirname, '..', '..', 'frontend', 'build', 'static')))
+      .get('*', function(req, res) {
+        res.sendFile('index.html', 
+            {root:  path.join(__dirname, '..', '..', 'frontend', 'build')})
+      })
+  );
+  frontend.listen(3020, () => {
+    console.log('Frontend Running at http://localhost:3020');
+  });
+});
+
+afterAll((done) => {
+  backend.close(() => { 
+    frontend.close(done);
+  });
+});
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--headless',
+    ],
+  });
+  page = await browser.newPage();
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+// Clicks the 'Get Dummy' button and checks the server response is displayed.
+test('input text into email field', async () => {
+  await page.goto('http://localhost:3020');
+  await page.type('input[name=email]', 'molly@slugmail.com');
+  await page.type('input[name=password]', 'mollymember');
+  await page.click('input[type=submit]');
+  await page.waitForNavigation();
+  let label = await page.$('#root > div > div >' +
+  'div.MuiBox-root.css-i9gxme > header > div > div');
+  cont = await (await label.getProperty('textContent')).jsonValue();
+  expect(cont).toBe('CSE 186');
+});
+
